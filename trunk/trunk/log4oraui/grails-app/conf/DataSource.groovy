1b8f980707993f0c49e2b4c0e9551c3d4c2dd40e
fly.yamaha <fly.yamaha@3eaf94a0-3527-11de-96ab-f16d9912eac9>
2009-10-14 02:18:06 +0000

    Inititial check in

---
+++ trunk/trunk/log4oraui/grails-app/conf/DataSource.groovy
@@ -0,0 +1,35 @@
+dataSource {
+	pooled = true
+	driverClassName = "org.hsqldb.jdbcDriver"
+	username = "sa"
+	password = ""
+    //loggingSql = 'true'
+  logSql = 'true'
+}
+hibernate {
+    cache.use_second_level_cache=true
+    cache.use_query_cache=true
+    cache.provider_class='com.opensymphony.oscache.hibernate.OSCacheProvider'
+}
+// environment specific settings
+environments {
+	development {
+		dataSource {
+			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+			url = "jdbc:hsqldb:mem:devDB"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:hsqldb:mem:testDb"
+        //  loggingSql = 'true'
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+		}
+	}
+}
